e402568a4cd448a0c3f50ce2c56739881728dc41
Serhii_Hulii <serhii_hulii@epam.com>
2022-12-06 12:51:39 +0300

    update watch branch

--- src/components/t-watch/t-watch-wrapper.vue
+++ src/components/t-watch/t-watch-wrapper.vue
@@ -2,12 +2,19 @@
   <div>
     <h1 class="green">watch</h1>
 
-    <Watch :propNumber="1" />
+    <Watch :propNumber="propNumber" />
   </div>
 </template>
 
 <script setup lang="ts">
 import Watch from "./t-watch.vue";
+import { ref } from "vue";
+
+const propNumber = ref(1);
+
+setInterval(() => {
+  propNumber.value = propNumber.value + 1;
+}, 5000);
 </script>
 
 <style scoped></style>
--- src/components/t-watch/t-watch.vue
+++ src/components/t-watch/t-watch.vue
@@ -1,25 +1,43 @@
 <template>
   <div>
-    <h1 class="green">watch</h1>
+    <p>propNumber: {{ propNumber }}</p>
+    <p>dataNumber: {{ dataNumber }}</p>
+    <p>computedNumber: {{ computedNumber }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
-import { watch } from "vue";
+import { computed, ref, watch } from "vue";
 
-let props = defineProps({
+let { propNumber } = defineProps({
   propNumber: {
     type: Number,
     required: true,
   },
 });
 
+let dataNumber = ref(2);
+
+let computedNumber = computed(() => dataNumber.value * 2);
+
 watch(
-  () => props.propNumber,
+  () => propNumber,
   (v) => {
     console.log(`new 'propNumber' = ${v}`);
   }
 );
+
+watch(dataNumber, (v) => {
+  console.log(`new 'dataNumber' = ${v}`);
+});
+
+watch(computedNumber, (v) => {
+  console.log(`new 'computedNumber' = ${v}`);
+});
+
+setInterval(() => {
+  dataNumber.value = dataNumber.value + 2;
+}, 5000);
 </script>
 
 <style scoped></style>
