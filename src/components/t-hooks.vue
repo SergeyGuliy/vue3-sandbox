fba6b23c8b919d13708c44832844d72658a0aa71
Serhii_Hulii <serhii_hulii@epam.com>
2022-12-06 13:38:48 +0300

    added hooks

---
+++ src/components/t-hooks.vue
@@ -0,0 +1,57 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+import {
+  onMounted,
+  onBeforeMount,
+  onActivated,
+  onDeactivated,
+  onRenderTracked,
+  onRenderTriggered,
+  onServerPrefetch,
+  onUpdated,
+  onBeforeUpdate,
+} from "vue";
+
+onBeforeMount(() => {
+  console.log(`onBeforeMount`);
+});
+onMounted(() => {
+  console.log(`onMounted`);
+});
+
+onBeforeUpdate(() => {
+  console.log(`onBeforeUpdate`);
+});
+onUpdated(() => {
+  console.log(`onUpdated`);
+});
+
+onRenderTracked(() => {
+  console.log(`onRenderTracked`);
+});
+onRenderTriggered(() => {
+  console.log(`onRenderTriggered`);
+});
+onServerPrefetch(() => {
+  console.log(`onServerPrefetch`);
+});
+onServerPrefetch(() => {
+  console.log(`onServerPrefetch`);
+});
+
+onActivated(() => {
+  console.log(`onActivated`);
+});
+onDeactivated(() => {
+  console.log(`onDeactivated`);
+});
+
+onMounted(() => {
+  console.log(`onBeforeUnmount`);
+});
+</script>
+
+<style scoped></style>
