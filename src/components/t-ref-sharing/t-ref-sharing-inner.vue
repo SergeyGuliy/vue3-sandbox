1ed3e63b97de992aabb3aef2f83bc2a745a24fb5
Serhii_Hulii <serhii_hulii@epam.com>
2022-12-08 18:39:36 +0300

    created ref sharing logic

---
+++ src/components/t-ref-sharing/t-ref-sharing-inner.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+import { getCurrentInstance, ref } from "vue";
+
+const instance: any = getCurrentInstance();
+console.warn("parent");
+console.log(instance.parent);
+console.warn("root");
+console.log(instance.root);
+
+const innerValue = ref("inner value");
+
+defineExpose({
+  innerValue,
+});
+</script>
+
+<style scoped></style>
---
+++ src/components/t-ref-sharing/t-ref-sharing-wrapper.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 class="green">ref sharing</h1>
+
+    <RefSharingInner fer="RefSharingInnerRef" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import RefSharingInner from "./t-ref-sharing-inner.vue";
+
+const outerValue = ref("outer value");
+
+const RefSharingInnerRef = ref("RefSharingInnerRef");
+
+console.warn('RefSharingInnerRef.value');
+console.log(RefSharingInnerRef.value);
+
+defineExpose({
+  outerValue,
+});
+</script>
+
+<style scoped></style>
